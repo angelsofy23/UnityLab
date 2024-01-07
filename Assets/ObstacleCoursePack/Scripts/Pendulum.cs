5b718e37b9678421e7d70ea8816e30105bc6753f
angelsofy23 <sofiabilyk23@gmail.com>
2024-01-07 17:20:36 +0000

    ...
    .

---
+++ Assets/ObstacleCoursePack/Scripts/Pendulum.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+	[SerializeField] float speed;
+	public float limit;
+
+    void Update()
+    {
+		float angle = limit * Mathf.Sin(Time.time * speed);
+		transform.localRotation = Quaternion.Euler(0, 0, angle);
+	}
+}
