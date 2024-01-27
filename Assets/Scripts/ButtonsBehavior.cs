dabfc22d31922ded0a17c358d86a8f542b67ea00
angelsofy23 <sofiabilyk23@gmail.com>
2024-01-27 20:45:06 +0000

    Botões e menus, inicial e pausa, feitos
    .

--- Assets/Scripts/ButtonsBehavior.cs
+++ Assets/Scripts/ButtonsBehavior.cs
@@ -6,7 +6,7 @@ public class ButtonsBehavior : MonoBehaviour
     [SerializeField] GameObject chooseplayers;
     [SerializeField] GameObject menu;
     [SerializeField] GameObject credits;
-
+    [SerializeField] GameObject menupausa;
 
     public void Choose()
     {
@@ -48,4 +48,34 @@ public class ButtonsBehavior : MonoBehaviour
         chooseplayers.SetActive(false);
         credits.SetActive(true);
     }
+
+    public void Continuar()
+    {
+        menupausa.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Pausa()
+    {
+        menupausa.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void VoltarHomeJogo()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuInicial");
+    }
+
+    public void RepetirPlayer1()
+  {
+    SceneManager.LoadScene("Player1");
+    Time.timeScale = 1f;
+  }
+
+   public void RepetirPlayer2()
+  {
+    SceneManager.LoadScene("Player2");
+    Time.timeScale = 1f;
+  }
 }
